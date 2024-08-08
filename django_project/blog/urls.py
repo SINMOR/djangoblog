b46f007fb1e053a5462a39b70a2325a5dbddbf1d
Sindet <morrisindet@gmail.com>
2024-08-08 17:17:54 +0300

    Application and routes

--- django_project/blog/urls.py
+++ django_project/blog/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
+from . import views
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path("", views.home, name="blog-home"),  #make sure the naming is specific so as not to colide with other app 
+    path("about/", views.about, name="blog-about"),  #make sure the naming is specific so as not to colide with other app 
 ]
