f61f8e761b465e5551611eee385e63a455a54fcc
Sindet <morrisindet@gmail.com>
2024-08-14 17:56:17 +0300

    Style the home page using tailwind

--- django_project/blog/urls.py
+++ django_project/blog/urls.py
@@ -1,7 +1,7 @@
-from django.urls import path
+from django.urls import path , include
 from . import views
 
 urlpatterns = [
-    path("", views.home, name="blog-home"),  #make sure the naming is specific so as not to colide with other app 
-    path("about/", views.about, name="blog-about"),  #make sure the naming is specific so as not to colide with other app 
+    path("", views.home, name="blog-home"),  #make sure the naming is specific so as not to collide with other app 
+    path("about/", views.about, name="blog-about"),  #make sure the naming is specific so as not to collide with other app 
 ]
--- project/blog/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("", views.home, name="blog-home"),  #make sure the naming is specific so as not to colide with other app 
-    path("about/", views.about, name="blog-about"),  #make sure the naming is specific so as not to colide with other app 
-]
