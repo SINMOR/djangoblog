ad3afff71e9da01c7732f7c1a6c49383e116ba75
Sindet <morrisindet@gmail.com>
2024-08-15 17:45:30 +0300

    added django admin and introduced database

--- django_project/blog/models.py
+++ django_project/blog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User 
 
-# Create your models here.
+
+# each class is its own table in the database and each attribute will be a different field in the database
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    date_posted = models.DateTimeField(
+        default=timezone.now
+    )  # sets the current datetime only when a post is added
+    author = models.ForeignKey(User, on_delete=models.CASCADE) # if  a user is deleted the post is also deleted 
